b3260a6e9329dbe8c4fe44ce350c572179691bc3
Studawatta <st.udawatte@gmail.com>
2023-05-16 23:12:10 +0530

    slider completed

--- client/src/features/slices/sliderSlice.jsx
+++ client/src/features/slices/sliderSlice.jsx
@@ -1,17 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { sliderData } from '../../assets/data/dummyData';
 
 export const sliderSlice = createSlice({
   name: 'slider',
   initialState: {
     value: 0,
-    length: 4,
+    length: sliderData.length,
   },
   reducers: {
     nextSlide(state, action) {
-      console.log;
+      console.log('action', action.payload);
+      console.log('state', state.value);
+      state.value = action.payload > state.length - 1 ? 0 : action.payload;
     },
-    prevSlide() {},
-    dotSlide() {},
+    prevSlide(state, action) {
+      state.value = action.payload < 0 ? state.length - 1 : action.payload;
+    },
+    dotSlide(state, action) {
+      const slide = action.payload;
+      console.log('dot', slide);
+      state.value = slide;
+    },
   },
 });
 
